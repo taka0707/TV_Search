9b021a1f4f990c3a0ba879dafb4f83a23e526640
taka0707 <yukken777@gmail.com>
2020-08-07 17:04:29 +0900

    edit-user.rb
    - user.rbにvalidation設定
    - application_controllerにdeviseのストロングパラメーター設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :password
+  end
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }, confirmation: true
+  validates :password_confirmation, presence: true
 end
