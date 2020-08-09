d5063187a624cc203ce253f3631fe31e83423ac6
taka0707 <yukken777@gmail.com>
2020-08-09 15:14:38 +0900

    edit-tv_schedules_controller
    - gem(simple_calendar)導入
    - simple_calendarの部分テンプレート画面作成(html&scss)
    - simple_calendarの部分テンプレートを用いてtv_schedulesのindex/search画面作成
    - searchのアクション&ルーティング設定

--- app/controllers/tv_schedules_controller.rb
+++ app/controllers/tv_schedules_controller.rb
@@ -3,27 +3,32 @@ class TvSchedulesController < ApplicationController
   require 'chronic'
 
   def index
+    
+  end
+
+  def search
     agent = Mechanize.new
     page = agent.get("https://tv.yahoo.co.jp/listings/realtime/")
     form = page.forms[0]
-    form.q = '心霊'
+    word = tv_schedule_params[:keyword]
+    form.q = word
     page = agent.submit(form)
     
     date_docs = page.css(".leftarea > p > em")
-    title_docs = page.css(".rightarea > .yjLS > a")
-    channel_docs = page.css(".rightarea > p > .pr35")
-    DateTimeEdit(date_docs)
-    TitleEdit(title_docs)
-    ChannelEdit(channel_docs)
-    @tvs = []
-    @dates.zip(@titles, @channels) do |date, title, channel|
-      @tv = TvSchedule.new
-      @tv.start_time = date
-      @tv.title = title
-      @tv.channel = channel
-      @tv.user_id = current_user.id
-      @tvs << @tv
-      # binding.pry
+    if date_docs.present?
+      title_docs = page.css(".rightarea > .yjLS > a")
+      channel_docs = page.css(".rightarea > p > .pr35")
+      DateTimeEdit(date_docs)
+      TitleEdit(title_docs)
+      ChannelEdit(channel_docs)
+      @tvs = []
+      @dates.zip(@titles, @channels) do |date, title, channel|
+        @tv = TvSchedule.new
+        @tv.start_time = date
+        @tv.title = title
+        @tv.channel = channel
+        @tvs << @tv
+      end
     end
   end
 
@@ -49,5 +54,11 @@ class TvSchedulesController < ApplicationController
     channel_docs.each do |doc3|
       @channels << doc3.text unless doc3.values.include?("pr35 floatl")
     end
+  end
+
+  private
+
+  def tv_schedule_params
+    params.permit(:keyword)
   end
 end
