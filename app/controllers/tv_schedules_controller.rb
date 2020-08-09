741d8e6c3b3aa48d173bb471b12dcaf78995f2f1
taka0707 <yukken777@gmail.com>
2020-08-09 17:26:36 +0900

    edit-tv_schedules_controller
    - tv_scheduleのsearchアクションに分岐追記(検索ワードが空白/検索結果なし)
    - フラッシュメッセージの部分テンプレート作成(html&scss)
    - applicationのviewファイルでフラッシュメッセージの部分テンプレート呼び出し

--- app/controllers/tv_schedules_controller.rb
+++ app/controllers/tv_schedules_controller.rb
@@ -3,32 +3,38 @@ class TvSchedulesController < ApplicationController
   require 'chronic'
 
   def index
-    
   end
 
   def search
-    agent = Mechanize.new
-    page = agent.get("https://tv.yahoo.co.jp/listings/realtime/")
-    form = page.forms[0]
     word = tv_schedule_params[:keyword]
-    form.q = word
-    page = agent.submit(form)
-    
-    date_docs = page.css(".leftarea > p > em")
-    if date_docs.present?
-      title_docs = page.css(".rightarea > .yjLS > a")
-      channel_docs = page.css(".rightarea > p > .pr35")
-      DateTimeEdit(date_docs)
-      TitleEdit(title_docs)
-      ChannelEdit(channel_docs)
-      @tvs = []
-      @dates.zip(@titles, @channels) do |date, title, channel|
-        @tv = TvSchedule.new
-        @tv.start_time = date
-        @tv.title = title
-        @tv.channel = channel
-        @tvs << @tv
+    if word.present?
+      agent = Mechanize.new
+      page = agent.get("https://tv.yahoo.co.jp/listings/realtime/")
+      form = page.forms[0]
+      form.q = word
+      page = agent.submit(form)     
+      date_docs = page.css(".leftarea > p > em")
+      if date_docs.present?
+        title_docs = page.css(".rightarea > .yjLS > a")
+        channel_docs = page.css(".rightarea > p > .pr35")
+        DateTimeEdit(date_docs)
+        TitleEdit(title_docs)
+        ChannelEdit(channel_docs)
+        @tvs = []
+        @dates.zip(@titles, @channels) do |date, title, channel|
+          @tv = TvSchedule.new
+          @tv.start_time = date
+          @tv.title = title
+          @tv.channel = channel
+          @tvs << @tv
+        end
+      else
+        flash.now[:alert] = '検索結果はありませんでした。'
+        render :index
       end
+    else
+      flash.now[:alert] = '文字を入力して下さい。'
+        render :index
     end
   end
 
