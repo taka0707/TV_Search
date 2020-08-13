9b3483de6abcd95ee7546574c85bb25c99c0eb65
taka0707 <yukken777@gmail.com>
2020-08-13 12:49:59 +0900

    update-events_controller
    - before_actionにログインしないとnew/search画面に入れないように設定
    - index/search画面のクラス名変更
    - カレンダーの週表示部分をその週のスタート月の表示に変更

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_time_zone, only: :DateTimeEdit
+  before_action :authenticate_user!, only: [:new, :search]
   require "nokogiri"
   require 'chronic'
 
